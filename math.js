589f5a7217e3b0af9556ab87ed978b8b60e43fe0
azizulhakims <it.azizulhakim@gmsil.com>
2022-08-07 12:17:03 +0600

    add math.js

---
+++ math.js
@@ -0,0 +1,18 @@
+const result = Math.pow(3, 8);
+console.log(result);
+
+
+const num1 = 25;
+const num2 = 29;
+
+const gap = Math.abs(num1-num2);
+
+console.log(gap);
+
+if( gap<5){
+    console.log('yes you can create friend');
+}
+
+else{
+    console.log('yes you can not friend')
+}
